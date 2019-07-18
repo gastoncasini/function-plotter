8dbb3fb2208df664ad72c60733139b7547d809bf
Gaston Casini <gastoncasini@gmail.com>
2019-07-18 13:15:17 -0300

    added graph grid

--- grafic.js
+++ grafic.js
@@ -34,40 +34,46 @@ function graphing() {
   // draw Axis
   c.beginPath();
   c.strokeStyle = 'white';
-  c.strokeWidth = '1px';
+  c.lineWidth = 2;
   c.moveTo(0, xAxis);
   c.lineTo(width, xAxis);
   c.moveTo(yAxis, 0);
   c.lineTo(yAxis, height);
+  c.stroke();
   // draw units
+  c.lineWidth = 0.25;
 
   while (x.position < x.end) {
     x.position = x.position + one;
-    c.moveTo(x.position, cero.x);
-    c.lineTo(x.position, cero.x + 5);
+    c.moveTo(x.position + 0.5, 0);
+    c.lineTo(x.position + 0.5, height);
   }
 
   while (x.position > x.start) {
     x.position = x.position - one;
-    c.moveTo(x.position, cero.x);
-    c.lineTo(x.position, cero.x + 5);
+    c.moveTo(x.position + 0.5, 0);
+    c.lineTo(x.position + 0.5, height);
   }
   while (y.position < y.end) {
     y.position = y.position + one;
-    c.moveTo(cero.y, y.position);
-    c.lineTo(cero.y + 5, y.position);
+    c.moveTo(0, y.position);
+    c.lineTo(width, y.position);
   }
   while (y.position > y.start) {
     y.position = y.position - one;
-    c.moveTo(cero.y, y.position);
-    c.lineTo(cero.y + 5, y.position);
+    c.moveTo(0, y.position + 0.5);
+    c.lineTo(width, y.position + 0.5);
   }
 
   c.stroke();
 }
 
 function funcGrapher(exp) {
+  c.lineWidth = 1;
   c.beginPath();
+  // map of the function
+  let values = [];
+
   // evaluate the expresion for all values of x
   // map canvas width to domain
   // map canvas height to codomain
@@ -76,12 +82,33 @@ function funcGrapher(exp) {
     let currentY = math.evaluate(exp, { x: currentX });
     let graphX = i;
     let graphY = cero.x - currentY * one;
+
+    // coditional for points out of the domain
+    if (i > 1) {
+      if (values[i - 1].y === Infinity || values[i - 1].y === -Infinity) {
+        console.log(currentX, currentY);
+        c.moveTo(graphX, graphY);
+      }
+    }
+
     if (i === 0) {
       c.moveTo(graphX, graphY);
     }
+
     c.lineTo(graphX, graphY);
+
+    const point = {
+      x: currentX,
+      y: currentY,
+      graphX,
+      graphY,
+    };
+
+    values.push(point);
   }
+
   c.stroke();
+  return values;
 }
 
 graphing();
@@ -90,6 +117,8 @@ graphing();
 
 const input = document.getElementById('input');
 input.addEventListener('change', e => {
+  // clear graph
   graphing();
-  funcGrapher(e.target.value);
+  // graph function
+  console.log(funcGrapher(e.target.value));
 });
